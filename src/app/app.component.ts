388d5703d5c0be8ef6a9c385aa4fd01e12b6977d
Administrator <Administrator@LUCIA>
2022-01-27 10:40:17 -0300

    Adding initial files.

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Todo } from './Todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo';
+  todos : Todo[] = [];
+  newTodo : string;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted=true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    }else{
+      alert('Please enter Todo');
+    }
+  }
+
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+}
